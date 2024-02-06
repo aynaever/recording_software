2e9dea58e8cad85e7a23a4efcd4db140e4c72380
Aymane <aymane.naouadir18@gmail.com>
2024-02-07 00:38:42 +0100

    add stop video functionality

--- main.py
+++ main.py
@@ -7,21 +7,33 @@ media = instance.media_new("v4l2:///dev/video0")
 player.set_media(media)
 
 
+# Play video functions
 def play_video():
     player.play()
 
 
+# Pause video functions
 def pause_video():
-    player.pause();
+    player.pause()
+
+
+# Stop video functions
+def stop_video():
+    player.stop()
 
 
 root = tk.Tk()
 root.title("Recording Program")
 
+# media control buttons
 start_button = tk.Button(root, text="Start", command=play_video)
 pause_button = tk.Button(root, text="Pause", command=pause_video)
+stop_button = tk.Button(root, text="Stop", command=stop_video)
+
+# add buttons to the gui
 start_button.pack()
 pause_button.pack()
+stop_button.pack()
 
 root.mainloop()
 player.stop()
