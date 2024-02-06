e65b22a3f89b296fc72a1af00c8476dd946cd908
Aymane <aymane.naouadir18@gmail.com>
2024-02-07 00:56:06 +0100

    create recordings directory

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import tkinter as tk
 import vlc
+import os
 
 # vlc, media, player instances
 instance = vlc.Instance()
 player = instance.media_player_new()
 media = instance.media_new("v4l2:///dev/video0")
 player.set_media(media)
+
+# Set options for recording
+options = ":sout=#transcode{vcodec=h264}:std{access=file,mux=mp4,dst=output.mp4}"
+
+# Add options to media
+media.add_option(options)
 
 
 # Play video functions
@@ -23,6 +30,16 @@ def stop_video():
     player.stop()
 
 
+# Create recordings directory
+def create_recording_dir():
+    dir_path = "./recordings/"
+    try:
+        os.mkdir(dir_path)
+        print(f"Directory '{dir_path}' created successfully!")
+    except FileExistsError:
+        print(f"Error: Directory '{dir_path}' already exists.")
+
+
 root = tk.Tk()
 root.title("Recording Program")
 
@@ -37,4 +54,5 @@ pause_button.pack()
 stop_button.pack()
 
 root.mainloop()
+create_recording_dir()
 player.stop()
